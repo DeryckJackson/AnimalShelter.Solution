80d91385785f893eb813b0c2dadd6c442c5792ea
Kate Skorija <kateskorija@gmail.com>
2020-07-28 10:28:49 -0700

    create animal model
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,11 @@
+namespace AnimalShelter.Models
+{
+  public class AnimalShelter
+  {
+    public int AnimalId { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public DateTime AdmittanceDate { get; set; }
+    public string Breed { get; set; }
+  }
+}
